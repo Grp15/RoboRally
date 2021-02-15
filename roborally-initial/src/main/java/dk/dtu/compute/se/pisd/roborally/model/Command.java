9a251d7280c4695a37ebf32ac6c140b0b4b3f7b3
s205436 <s205436@student.dtu.dk>
2021-02-15 14:10:30 +0100

    Added new Command enumeration from V3 assignment

--- roborally-initial/src/main/java/dk/dtu/compute/se/pisd/roborally/model/Command.java
+++ roborally-initial/src/main/java/dk/dtu/compute/se/pisd/roborally/model/Command.java
@@ -38,12 +38,33 @@ public enum Command {
     FORWARD("Fwd"),
     RIGHT("Turn Right"),
     LEFT("Turn Left"),
-    FAST_FORWARD("Fast Fwd");
+    FAST_FORWARD("Fast Fwd"),
+
+    // XXX Assignment V3
+    OPTION_LEFT_RIGHT("Left OR Right", LEFT, RIGHT);
 
     final public String displayName;
 
-    Command(String displayName) {
+    // XXX Assignment V3
+    // Command(String displayName) {
+    //     this.displayName = displayName;
+    // }
+    //
+    // replaced by the code below:
+
+    final private List<Command> options;
+
+    Command(String displayName, Command... options) {
         this.displayName = displayName;
+        this.options = Collections.unmodifiableList(Arrays.asList(options));
+    }
+
+    public boolean isInteractive() {
+        return !options.isEmpty();
+    }
+
+    public List<Command> getOptions() {
+        return options;
     }
 
 }
