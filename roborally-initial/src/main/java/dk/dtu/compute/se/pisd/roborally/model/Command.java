868df395a6172a392195763b64aae13b62825c51
s205436 <s205436@student.dtu.dk>
2021-02-22 17:13:42 +0100

    Java-Doc made for Board and Command.

--- roborally-initial/src/main/java/dk/dtu/compute/se/pisd/roborally/model/Command.java
+++ roborally-initial/src/main/java/dk/dtu/compute/se/pisd/roborally/model/Command.java
@@ -26,53 +26,62 @@ import java.util.Collections;
 import java.util.List;
 
 /**
- * ...
+ * The Command class is responsible for the different commands for the CommandCards.
  *
  * @author Ekkart Kindler, ekki@dtu.dk
- *
+ * @author s205436
+ * @author s164539
+ * @author s152780
+ * @author s205472
+ * @author s194612
  */
 public enum Command {
-
     // This is a very simplistic way of realizing different commands.
-
     FORWARD("Fwd"),
     RIGHT("Turn Right"),
     LEFT("Turn Left"),
     FAST_FORWARD("Fast Fwd"),
 
-    // XXX Assignment V3
     OPTION_LEFT_RIGHT("Left OR Right", LEFT, RIGHT);
 
     final public String displayName;
 
-    // XXX Assignment V3
-    // Command(String displayName) {
-    //     this.displayName = displayName;
-    // }
-    //
-    // replaced by the code below:
-
     final private List<Command> options;
-
 
     /**
      * Displays the name of the command along with a list of options
-     * @param displayName
-     * @param options
+     * @param displayName name of command
+     * @param options list of options
      */
     Command(String displayName, Command... options) {
         this.displayName = displayName;
         this.options = Collections.unmodifiableList(Arrays.asList(options));
     }
 
+    /**
+     * Method that tells if a command's options is interactive or not.
+     *
+     * @return true/false depending if interactive
+     */
     public boolean isInteractive() {
         return !options.isEmpty();
     }
 
+    /**
+     * Returns a list of Command options.
+     *
+     * @return options for command
+     */
     public List<Command> getOptions() {
         return options;
     }
 
+    /**
+     * @author s205436
+     * Returns display name for given command.
+     *
+     * @return displayname for command
+     */
     public String getDisplayName(){
         return displayName;
     }
