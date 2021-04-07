0b468f064aa49c6a427ef6028b823693b63c6948
Hildibjørg Didriksen <s164539@student.dtu.dk>
2021-04-07 14:18:56 +0200

    changes made in Repository

--- roborally-initial/src/main/java/dk/dtu/compute/se/pisd/roborally/dal/Repository.java
+++ roborally-initial/src/main/java/dk/dtu/compute/se/pisd/roborally/dal/Repository.java
@@ -564,52 +564,6 @@ class Repository implements IRepository {
 		return select_games_stmt;
 	}
 
-	private CommandCard[] LoadCommandCardsFromDisplayName(String[] Commands){
-		Command[] commands = Command.values();
-		CommandCard[] register;
-		register = new CommandCard[Commands.length];
-
-		System.out.println("Commands length " + Commands.length);
-
-
-		for(int i = 0; i < Commands.length - 1 ; i++) {
-
-			switch (Commands[i]) {
-				case "Fwd":
-					register[i] = new CommandCard(commands[0]);
-					System.out.println(i + Commands[i]);
-					break;
-
-				case "Fast Fwd":
-					register[i] = new CommandCard(commands[3]);
-					System.out.println(i + Commands[i]);
-					break;
-
-				case "Turn Right":
-					register[i] = new CommandCard(commands[1]);
-					System.out.println(i + Commands[i]);
-					break;
-
-				case "Turn Left":
-					register[i] = new CommandCard(commands[2]);
-					System.out.println(i + Commands[i]);
-					break;
-
-				case "Left OR Right":
-					register[i] = new CommandCard(commands[4]);
-					System.out.println(i + Commands[i]);
-					break;
-
-				default:
-					register[i] = null;
-					System.out.println(i + Commands[i]);
-					break;
-
-			}
-		}
-		return register;
-	}
-
 
 
 }
