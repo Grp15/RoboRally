22cf4f1879dcfb0a93e782806be92b07657780cc
Troels <s152780@student.dtu.dk>
2021-04-14 11:25:50 +0200

    Added StartFieldView

---
+++ roborally-initial/src/main/java/dk/dtu/compute/se/pisd/roborally/view/StartFieldView.java
@@ -0,0 +1,22 @@
+package dk.dtu.compute.se.pisd.roborally.view;
+
+import dk.dtu.compute.se.pisd.roborally.model.Space;
+import dk.dtu.compute.se.pisd.roborally.model.Spaces.ConveyorBelt;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+
+public class StartFieldView{
+    public static void drawConveyorBelt(SpaceView spaceView, Space space){
+        ConveyorBelt tempSpace = (ConveyorBelt) space;
+
+        Canvas canvas = new Canvas(SpaceView.SPACE_WIDTH, SpaceView.SPACE_HEIGHT);
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+        gc.setFill(Color.GREEN);
+        gc.fillOval(25,32,30, 20);
+        // TODO lave switch case for forskellige gear retninger
+
+        spaceView.getChildren().add(canvas);
+
+    }
+}
