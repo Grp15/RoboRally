c37224c86263ee08affd5a9d064ae99bd318a938
Troels <s152780@student.dtu.dk>
2021-04-14 09:15:34 +0200

    Tidied up GameController.java

--- roborally-initial/src/main/java/dk/dtu/compute/se/pisd/roborally/controller/GameController.java
+++ roborally-initial/src/main/java/dk/dtu/compute/se/pisd/roborally/controller/GameController.java
@@ -223,8 +223,6 @@ public class GameController {
                         board.setPhase(Phase.PLAYER_INTERACTION);
                         return;
                     } else executeCommand(currentPlayer, command);
-
-                    // Kan måske også indsættes her så der checkes efter der er udført en executeCommand
                 }
                 int nextPlayerNumber = board.getPlayerNumber(currentPlayer) + 1;
                 if (nextPlayerNumber < board.getPlayersNumber()) {
