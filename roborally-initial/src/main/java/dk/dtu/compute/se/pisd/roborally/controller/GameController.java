acbfaf5a3eca41bd75dd87e2c38a0303951e726e
Troels <s152780@student.dtu.dk>
2021-04-14 09:12:23 +0200

    Tidied up GameController.java

--- roborally-initial/src/main/java/dk/dtu/compute/se/pisd/roborally/controller/GameController.java
+++ roborally-initial/src/main/java/dk/dtu/compute/se/pisd/roborally/controller/GameController.java
@@ -53,13 +53,6 @@ public class GameController {
      * @param space the space to which the current player should move
      */
     public void moveCurrentPlayerToSpace(@NotNull Space space)  {
-        // TODO Assignment V1: method should be implemented by the students:
-        //   - the current player should be moved to the given space
-        //     (if it is free()
-        //   - and the current player should be set to the player
-        //     following the current player
-        //   - the counter of moves in the game should be increased by one
-        //     if the player is moved
 
         Player current = board.getCurrentPlayer();
         if(current == null) return;
@@ -249,23 +242,6 @@ public class GameController {
                         space.doAction(player,space,gameController);
 
                         System.out.println(space.getSpaceType());
-
-
-                        /*
-                        int currentInt = board.getPlayerNumber(board.getPlayer(i));
-
-
-                        Player nextPlayer = board.getPlayer((currentInt + 1) % board.getPlayersNumber());
-
-
-
-                        System.out.println(nextPlayer.board.getPlayersNumber() +" " + currentInt);
-
-                        space.doAction(board.getPlayer(currentInt), space, gameController);
-
-                         */
-
-
 
                     }
 
