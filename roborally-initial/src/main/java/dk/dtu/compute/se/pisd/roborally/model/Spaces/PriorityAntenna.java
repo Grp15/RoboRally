a6530618a6373b554a891156c6d1d49863903bdb
Troels <troelstoy@outlook.com>
2021-04-20 00:28:57 +0200

    Added constructor and doAction to PriorityAntenna

--- roborally-initial/src/main/java/dk/dtu/compute/se/pisd/roborally/model/Spaces/PriorityAntenna.java
+++ roborally-initial/src/main/java/dk/dtu/compute/se/pisd/roborally/model/Spaces/PriorityAntenna.java
@@ -1,4 +1,29 @@
 package dk.dtu.compute.se.pisd.roborally.model.Spaces;
 
-public class PriorityAntenna {
+import dk.dtu.compute.se.pisd.designpatterns.observer.Subject;
+import dk.dtu.compute.se.pisd.roborally.controller.GameController;
+import dk.dtu.compute.se.pisd.roborally.model.Board;
+import dk.dtu.compute.se.pisd.roborally.model.Player;
+import dk.dtu.compute.se.pisd.roborally.model.Space;
+
+public class PriorityAntenna extends Space {
+
+    public PriorityAntenna(Board board, int x, int y) {
+        super(board, x, y);
+
+    }
+
+    /**
+     * Create in which order players turn is
+     * @param player
+     * @param space
+     * @param gameController
+     * @return
+     */
+
+    public boolean doAction(Player player, Space space, GameController gameController){
+
+
+        return true;
+    }
 }
