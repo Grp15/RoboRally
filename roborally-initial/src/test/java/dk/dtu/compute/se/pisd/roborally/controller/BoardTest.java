cd719cf6e7d7ae9bbd39d8455943aabe22f865a7
Troels <s152780@student.dtu.dk>
2021-04-21 12:50:00 +0200

    Created test class for board

---
+++ roborally-initial/src/test/java/dk/dtu/compute/se/pisd/roborally/controller/BoardTest.java
@@ -0,0 +1,41 @@
+package dk.dtu.compute.se.pisd.roborally.controller;
+
+import dk.dtu.compute.se.pisd.roborally.model.Board;
+import dk.dtu.compute.se.pisd.roborally.model.Heading;
+import dk.dtu.compute.se.pisd.roborally.model.Player;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+public class BoardTest {
+
+    private final int TEST_WIDTH = 8;
+    private final int TEST_HEIGHT = 8;
+
+    private GameController gameController;
+
+    /**
+     * Sets up the gameController with board, players, space and heading
+     */
+    @BeforeEach
+    void setUp() {
+        Board board = new Board(TEST_WIDTH, TEST_HEIGHT);
+        gameController = new GameController(board);
+        for (int i = 0; i < 6; i++) {
+            Player player = new Player(board, null,"Player " + i);
+            board.addPlayer(player);
+            player.setSpace(board.getSpace(1,i));
+            player.setHeading(Heading.values()[(i + 1) % Heading.values().length]);
+        }
+        board.setCurrentPlayer(board.getPlayer(0));
+    }
+
+    /**
+     * Tears down the gameController after each test
+     */
+    @AfterEach
+    void tearDown() {
+        gameController = null;
+    }
+
+
+}
