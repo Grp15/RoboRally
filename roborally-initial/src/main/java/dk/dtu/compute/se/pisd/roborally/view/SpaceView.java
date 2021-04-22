ece2b6038978bc59535c9b4250f08ff3acce2a26
Hildibjørg Didriksen <s164539@student.dtu.dk>
2021-04-22 17:32:45 +0200

    Updated the updateView() so the Player is on top of the elements on the board

--- roborally-initial/src/main/java/dk/dtu/compute/se/pisd/roborally/view/SpaceView.java
+++ roborally-initial/src/main/java/dk/dtu/compute/se/pisd/roborally/view/SpaceView.java
@@ -72,7 +72,7 @@ public class SpaceView extends StackPane implements ViewObserver {
     }
 
     private void updatePlayer() {
-        this.getChildren().clear();
+        //this.getChildren().clear(); // This clears all elements on the board and if removed the player can be placed on top of the elements
 
         Player player = space.getPlayer();
         if (player != null) {
@@ -93,16 +93,19 @@ public class SpaceView extends StackPane implements ViewObserver {
     @Override
     public void updateView(Subject subject) {
         if (subject == this.space) {
+
+           // updatePlayer(); // if the player if updated here, the other elements are placed over the player
+            if (this.space instanceof Gears) {
+                GearView.drawGear(this, space);
+            }
+            if (this.space instanceof ConveyorBelt) {
+                ConveyorBeltView.drawConveyorBelt(this, space);
+            }
+            if (this.space instanceof StartField) {
+                StartFieldView.drawStartField(this, space);
+            }
             updatePlayer();
-        }
-        if (this.space instanceof Gears) {
-            GearView.drawGear(this, space);
-        }
-        if (this.space instanceof ConveyorBelt) {
-            ConveyorBeltView.drawConveyorBelt(this, space);
-        }
-        if(this.space instanceof StartField) {
-            StartFieldView.drawStartField(this,space);
+
         }
     }
 
