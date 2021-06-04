b0eceab580e08e9e7f51bf1a578051b5c8cbf07b
Troels <troelstoy@outlook.com>
2021-06-04 16:16:22 +0200

    Added constructor CheckPoints

--- roborally-initial/src/main/java/dk/dtu/compute/se/pisd/roborally/model/Spaces/CheckPoints.java
+++ roborally-initial/src/main/java/dk/dtu/compute/se/pisd/roborally/model/Spaces/CheckPoints.java
@@ -5,9 +5,12 @@ import dk.dtu.compute.se.pisd.roborally.model.Space;
 
 public class CheckPoints extends Space {
     private SpaceType CHECKPOINT;
-    private String name;
+    private int Number; // Skal have et nr. op til det max antal der kan være
 
-    public CheckPoints(Board board, int x, int y) {
+    public CheckPoints(Board board, int x, int y, int Number) {
         super(board, x, y);
+        this.Number = Number;
     }
+
+
 }
