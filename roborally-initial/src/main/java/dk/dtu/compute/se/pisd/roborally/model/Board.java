cfb5187fbd79aec87cd6c75e5b1b008efcff6199
Troels <troelstoy@outlook.com>
2021-06-07 09:48:45 +0200

    Now shows the number of checkpoints the player has reached, can be done more elegantly

--- roborally-initial/src/main/java/dk/dtu/compute/se/pisd/roborally/model/Board.java
+++ roborally-initial/src/main/java/dk/dtu/compute/se/pisd/roborally/model/Board.java
@@ -267,7 +267,7 @@ public class Board extends Subject {
         //      which is counted up every time a player makes a move; the
         //      status line should show the current player and the number
         //      of the current move!
-        return "Player = " + getCurrentPlayer().getName() + ", Number of moves = " + getCounter();
+        return "Player = " + getCurrentPlayer().getName() + ", Number of moves = " + getCounter() +" " + "Number of checkpoints= " + getCurrentPlayer().getCheckPoints();
     }
 
     // TODO Assignment V1: add a counter along with a getter and a setter, so the
