6c108d34c547484ff3e12e7a01173c73051b8c3c
Hildibjørg Didriksen <s164539@student.dtu.dk>
2021-06-08 15:45:10 +0200

    removed space type from conveyorbelt

--- roborally-initial/src/main/java/dk/dtu/compute/se/pisd/roborally/model/Spaces/ConveyorBelt.java
+++ roborally-initial/src/main/java/dk/dtu/compute/se/pisd/roborally/model/Spaces/ConveyorBelt.java
@@ -1,5 +1,6 @@
 package dk.dtu.compute.se.pisd.roborally.model.Spaces;
 
+import dk.dtu.compute.se.pisd.roborally.controller.FieldAction;
 import dk.dtu.compute.se.pisd.roborally.controller.GameController;
 import dk.dtu.compute.se.pisd.roborally.model.*;
 import org.jetbrains.annotations.NotNull;
@@ -7,12 +8,11 @@ import org.jetbrains.annotations.NotNull;
 public class ConveyorBelt extends Space {
 
     private Heading heading;
-    private SpaceType type = SpaceType.CONVEYORBELT;
+//    private SpaceType type = SpaceType.CONVEYORBELT;
 
     public ConveyorBelt(Board board, int x, int y, Heading heading) {
         super(board, x, y);
         this.heading = heading;
-
     }
 
     public Heading getHeading() {
@@ -23,10 +23,13 @@ public class ConveyorBelt extends Space {
         this.heading = heading;
     }
 
+    /*
     public SpaceType getSpaceType(){
         return type;
     }
 
+
+     */
     public boolean doAction(@NotNull Player player, @NotNull Space space, GameController gameController) {
         // TODO Skub en spiller i den retning ConveyorBelt peger. (Husk at spillet eksekvere 1 register pr. spiller ad gangen)
 
