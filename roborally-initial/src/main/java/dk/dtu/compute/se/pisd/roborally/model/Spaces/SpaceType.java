5aa6ba564b22d8b3ec2d624a408f577dd4d2d9a7
Hildibjørg Didriksen <s164539@student.dtu.dk>
2021-06-08 14:08:34 +0200

    Added Checkpoint to SpaceType class

--- roborally-initial/src/main/java/dk/dtu/compute/se/pisd/roborally/model/Spaces/SpaceType.java
+++ roborally-initial/src/main/java/dk/dtu/compute/se/pisd/roborally/model/Spaces/SpaceType.java
@@ -1,6 +1,5 @@
 package dk.dtu.compute.se.pisd.roborally.model.Spaces;
 
 public enum SpaceType {
-    CONVEYORBELT, SPACE, GEARS, STARTFIELD, PRIORITY_ANTENNA,
+    CONVEYORBELT, SPACE, GEARS, STARTFIELD, PRIORITY_ANTENNA,CHECKPOINT
 }
-
