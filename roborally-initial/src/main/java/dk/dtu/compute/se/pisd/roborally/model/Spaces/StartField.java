93fa94f9629ef2fe0531f57fa95357798906bb53
Hildibjørg Didriksen <s164539@student.dtu.dk>
2021-06-09 14:04:42 +0200

    removed spacetypes and it's getter

--- roborally-initial/src/main/java/dk/dtu/compute/se/pisd/roborally/model/Spaces/StartField.java
+++ roborally-initial/src/main/java/dk/dtu/compute/se/pisd/roborally/model/Spaces/StartField.java
@@ -5,14 +5,16 @@ import dk.dtu.compute.se.pisd.roborally.model.Space;
 
 public class StartField extends Space {
 
-    private SpaceType type = SpaceType.STARTFIELD;
+   // private SpaceType type = SpaceType.STARTFIELD;
 
     public StartField(Board board, int x, int y) {
         super(board, x, y);
 
     }
-
+/*
     public SpaceType getSpaceType(){
         return type;
     }
+
+ */
 }
