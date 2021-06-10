15c6fea57d84191e3e17a49246415787d85ed76f
Hildibjørg Didriksen <s164539@student.dtu.dk>
2021-06-10 09:39:23 +0200

    Updated the URL for the images for StartField space

--- roborally-initial/src/main/java/dk/dtu/compute/se/pisd/roborally/view/StartFieldView.java
+++ roborally-initial/src/main/java/dk/dtu/compute/se/pisd/roborally/view/StartFieldView.java
@@ -1,12 +1,9 @@
 package dk.dtu.compute.se.pisd.roborally.view;
 
 import dk.dtu.compute.se.pisd.roborally.model.Space;
-import dk.dtu.compute.se.pisd.roborally.model.Spaces.ConveyorBelt;
-import dk.dtu.compute.se.pisd.roborally.model.Spaces.StartField;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
-import javafx.scene.paint.Color;
 
 public class StartFieldView{
     public static void drawStartField(SpaceView spaceView, Space space){
@@ -14,7 +11,7 @@ public class StartFieldView{
         Canvas canvas = new Canvas(SpaceView.SPACE_WIDTH, SpaceView.SPACE_HEIGHT);
         GraphicsContext gc = canvas.getGraphicsContext2D();
 
-        Image startImage = new Image("/startfield.png", 75, 75, false, false);
+        Image startImage = new Image("/pictures/startfield.png", 75, 75, false, false);
         gc.drawImage(startImage, 0, 0);
         canvas.setOpacity(0.6);
         spaceView.getChildren().add(canvas);
