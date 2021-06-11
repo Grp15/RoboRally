34b1b3893792aca7d1979a8da0bae57e00f6ea16
Hildibjørg Didriksen <s164539@student.dtu.dk>
2021-06-11 18:04:18 +0200

    Updated comment

--- roborally-initial/src/main/java/dk/dtu/compute/se/pisd/roborally/view/EnergyView.java
+++ roborally-initial/src/main/java/dk/dtu/compute/se/pisd/roborally/view/EnergyView.java
@@ -11,9 +11,6 @@ import javafx.scene.image.Image;
  * EnerguView is a class that loads the energy spaces png's from the picture resource folder and draws it on the spaces using canvas
  *
  * @author S164539
- * @author S154780
- * @author S205472
- * @author S194612
  */
 public class EnergyView {
 
@@ -23,9 +20,6 @@ public class EnergyView {
      * @param spaceView
      * @param space
      * @author S164539
-     * @author S154780
-     * @author S205472
-     * @author S194612
      */
     public static void drawEnergy(SpaceView spaceView, Space space){
         int energy = 0;
